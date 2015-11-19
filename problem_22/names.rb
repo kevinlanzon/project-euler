495ac94fcf26eca7ffb5f2c296abe40dd5ec0678
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-19 15:45:46 +0000

    adds method to get the value of letters in the alphabet

--- problem_22/names.rb
+++ problem_22/names.rb
@@ -3,3 +3,13 @@ def sort_names
   names = file.read.gsub(/\w+/) # reads file and uses regex to to replace backslashes from string
   names.sort # sort alphabetically names into an array
 end
+
+# COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+def letter_value
+  @map = {}
+  ('A'..'Z').each_with_index do |letter, index|
+    @map[letter] = index + 1
+  end
+  puts @map # method return a hash of key, value pairs for letters {"A"=>1, "B"=>2 etc...}
+end
