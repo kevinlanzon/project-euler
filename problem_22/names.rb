ad8b50648a28f54f9c57a02d13d1dcf7e4844410
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-19 16:38:09 +0000

    moves letter_value method into total_score method. All seems to work

--- problem_22/names.rb
+++ problem_22/names.rb
@@ -1,17 +1,7 @@
 def sort_names
   file = File.open(File.dirname(__FILE__) + '/p022_names.txt')
   names = file.read.gsub(/\w+/) # reads file and uses regex to to replace backslashes from string
-  names.sort # sort alphabetically names into an array
-end
-
-# COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
-
-def letter_value
-  @map = {}
-  ('A'..'Z').each_with_index do |letter, index|
-    @map[letter] = index + 1
-  end
-  puts @map # method return a hash of key, value pairs for letters {"A"=>1, "B"=>2 etc...}
+  names.sort # sort names alphabetically into an array
 end
 
 def name_score(name, index)
@@ -19,5 +9,21 @@ def name_score(name, index)
     sum + @map[char]
   end
   score * index
-  puts score # returns the total score for each name in the txt file
 end
+
+def total_score
+  @map = {}
+  ('A'..'Z').each_with_index do |letter, index|
+    @map[letter] = index + 1
+    # returns total score for each name
+  end
+  puts @map
+  names = sort_names
+  scores = []
+  names.each_with_index do |name, index|
+    scores << name_score(name, index + 1)
+  end
+  scores.inject { |sum, n| sum + n }
+end
+
+puts total_score # total score = 871198282
