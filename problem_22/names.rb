251bf6f1e7a968bfd6b823a46535e915c9358735
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-19 17:28:34 +0000

    refactors inject block into symbol argument

--- problem_22/names.rb
+++ problem_22/names.rb
@@ -1,6 +1,6 @@
 def sort_names
   File.open(File.dirname(__FILE__) + '/p022_names.txt').read.gsub(/\w+/).sort
-  # reads file and uses regex to to replace backslashes from string and sorts alphbetically
+  # reads file and uses regex to to replace backslashes from string and sorts alphbetically into array
 end
 
 def name_score(name, index)
@@ -8,21 +8,20 @@ def name_score(name, index)
     sum + @map[char]
   end
   score * index
+  # returns total score for each name
 end
 
 def total_score
   @map = {}
   ('A'..'Z').each_with_index do |letter, index|
     @map[letter] = index + 1
-    # returns total score for each name
   end
   names = sort_names
   scores = []
   names.each_with_index do |name, index|
     scores << name_score(name, index + 1)
   end
-  scores.inject { |sum, n| sum + n }
+  scores.inject(:+)
 end
 
 puts "The total of all the name scores in the file is #{total_score}" # total score = 871198282
-
