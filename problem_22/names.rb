28ebe0934dafb60d6b1837bc04692537893352c1
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-19 12:18:43 +0000

    sorts names txt file alphabetically into an array

--- problem_22/names.rb
+++ problem_22/names.rb
@@ -1,3 +1,5 @@
-def names
-  File.open(File.dirname(__FILE__) + '/p022_names.txt').read
+def sort_names
+  file = File.open(File.dirname(__FILE__) + '/p022_names.txt')
+  names = file.read.gsub(/\w+/) # reads file and uses regex to to replace backslashes from string
+  names.sort # sort alphabetically names into an array
 end
