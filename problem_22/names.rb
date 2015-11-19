e5aa76cf73ab9125cc4e1e16c9cbdd9c3b37ea00
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-19 21:54:44 +0000

    changes block argument in total_score method to more explicit name

--- problem_22/names.rb
+++ problem_22/names.rb
@@ -4,7 +4,7 @@ def sort_names
 end
 
 def name_score(name, index)
-  score = name.each_char.inject(0) do |sum, char|
+  score = name.each_char.inject(0) do |sum, char| # each_char passes each character in the string to the given block
     sum + @map[char]
   end
   score * index
@@ -13,15 +13,15 @@ end
 
 def total_score
   @map = {}
-  ('A'..'Z').each_with_index do |letter, index|
-    @map[letter] = index + 1
+  ('A'..'Z').each_with_index do |letter, value| # each_with_index calls block with two arguments, the item and its index.
+    @map[letter] = value + 1
   end
   names = sort_names
   scores = []
   names.each_with_index do |name, index|
     scores << name_score(name, index + 1)
   end
-  scores.inject(:+)
+  scores.inject(:+) # inject method sums all the names scores
 end
 
 puts "The total of all the name scores in the file is #{total_score}" # total score = 871198282
