572dbbd86bb3486de4b41bebd042f6ee93f6d17b
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-23 21:24:16 +0000

    problem 1 solved

--- problem_1/multiples.rb
+++ problem_1/multiples.rb
@@ -4,18 +4,20 @@ class Multiples
     @number = number
   end
 
-  def list_of_numbers
-    (1..@number).to_a
+  def natural_numbers
+    (1...@number).to_a
   end
 
   def of_three_and_five
-    @number % 3 == 0 && @number % 5 == 0
+    natural_numbers.select { |n| n % 3 == 0 || n % 5 == 0 }
+  end
+
+  def sum_of_all_multiples
+    num = of_three_and_five
+    num.inject(:+)
   end
 end
 
+calc = Multiples.new(1000)
 
-multiples = Multiples.new(23)
-
-puts multiples.list_of_numbers
-
-puts multiples.of_three_and_five
+puts calc.sum_of_all_multiples
