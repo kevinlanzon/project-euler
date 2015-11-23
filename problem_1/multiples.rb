a34167f0e5a1c7ba4508719cdfdf9c8ca820fe13
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-23 19:39:25 +0000

    adds method to find multiples of 3 and 5

--- problem_1/multiples.rb
+++ problem_1/multiples.rb
@@ -1,4 +1,15 @@
+class Multiples
+
+  def initialize(number)
+    @number = number
+  end
+
+  def of_three_and_five
+    @number % 3 == 0 && @number % 5 == 0
+  end
+end
 
 
+list = Multiples.new(15)
 
-multiples = Multiples.new(number)
+puts list.of_three_and_five
