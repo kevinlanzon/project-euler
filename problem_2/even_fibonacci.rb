4d15bb4d18805b665bd260aef19c0b263925efc9
Kevin Lanzon <kevinlanzon@gmail.com>
2015-11-24 17:04:23 +0000

    sorts even numbers in an array

--- problem_2/even_fibonacci.rb
+++ problem_2/even_fibonacci.rb
@@ -1,5 +1,21 @@
 class EvenFibonacci
 
+  def initialize(number)
+    @number = number
+  end
+
+  def array_of_numbers
+    (1..@number).to_a
+  end
+
+  def even_numbers
+    array_of_numbers.select { |n| n % 2 == 0 }
+  end
+
 end
 
-calc = EvenFibonacci.new
+calc = EvenFibonacci.new(10)
+
+puts calc.array_of_numbers
+
+puts calc.even_numbers
